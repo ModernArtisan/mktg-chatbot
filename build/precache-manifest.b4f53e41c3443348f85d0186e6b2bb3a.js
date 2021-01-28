457e7774a2ddbf2cbb95c76bece7e810495eece5
MArtisan <46476538+MArtisan@users.noreply.github.com>
2021-01-28 02:06:30 -0600

    add build

---
+++ build/precache-manifest.b4f53e41c3443348f85d0186e6b2bb3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1407ce7cec7d37370acb05e5b59b2f7d",
+    "url": "/ModernArtisan/mktg-chatbot.git/index.html"
+  },
+  {
+    "revision": "b88ab9fed989aa47a08a",
+    "url": "/ModernArtisan/mktg-chatbot.git/static/css/2.12da0d42.chunk.css"
+  },
+  {
+    "revision": "2e6694386e181e63269e",
+    "url": "/ModernArtisan/mktg-chatbot.git/static/css/main.9d14c3ba.chunk.css"
+  },
+  {
+    "revision": "b88ab9fed989aa47a08a",
+    "url": "/ModernArtisan/mktg-chatbot.git/static/js/2.01071dff.chunk.js"
+  },
+  {
+    "revision": "2e6694386e181e63269e",
+    "url": "/ModernArtisan/mktg-chatbot.git/static/js/main.dd505068.chunk.js"
+  },
+  {
+    "revision": "1d3abbc8db0806538654",
+    "url": "/ModernArtisan/mktg-chatbot.git/static/js/runtime~main.a79adefb.js"
+  },
+  {
+    "revision": "192e7ad859f174992ef66f9f0ff41f9c",
+    "url": "/ModernArtisan/mktg-chatbot.git/static/media/center-stage.192e7ad8.png"
+  }
+]);
